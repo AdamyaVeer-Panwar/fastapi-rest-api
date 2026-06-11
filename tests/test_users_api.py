b5727d54993ac0c8f9eee3cfda2afb01f46cc9c0
AdamyaVeer Panwar <224821405+AdamyaVeer-Panwar@users.noreply.github.com>
2026-06-11 18:55:30 +0530

    add user api integration test

---
+++ tests/test_users_api.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_create_user():
+
+    response = client.post(
+        "/users",
+        json={
+            "name": "Test User",
+            "email": "test@example.com"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["name"] == "Test User"
+    assert data["email"] == "test@example.com"
